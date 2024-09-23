114efdfc2d846dd0ace26da0a9e4e67abd59ad7b
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-09-23 19:09:08 +0100

    changes made

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import productsRoutes from './routes/products_routes.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hello World!");
+app.use(express.json());
+
+mongoose.connect(process.env.db).then((response) => {
+    console.log('Database Connected!');
+    app.listen(process.env.PORT, () => {
+        console.log('Server is listening on port 3000!');
+    });
+}).catch((error) => {
+    console.log(error);
+});
+
+app.get((req, res,) => {
+    res.json({message: 'Hello World'})
+})
+
+app.use('/api/products', productsRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error!';
+
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    });
 });
