cd304772517e53dfa82e7eaf334afa35a0918a83
Abdul-Quadri Mahmud Adeyemi <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2024-09-25 00:49:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://adexify.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
