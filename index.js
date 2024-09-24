43f9dbcd054e908163feb7c6942aea3a0958518c
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-09-24 21:28:09 +0100

    changes made

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const corsOptions = {
  
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 mongoose.connect(process.env.db).then((response) => {
     console.log('Database Connected!');
     app.listen(process.env.PORT, () => {
